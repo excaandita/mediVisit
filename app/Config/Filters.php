3a7e6eb4c880661571f35699d22ed21da7430c6b
exca <excamuchlisandita@gmail.com>
2024-07-29 09:42:16 +0700

    Filter Auth

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -107,6 +107,6 @@ class Filters extends BaseFilters
      * @var array<string, array<string, list<string>>>
      */
     public array $filters = [
-        'auth' => ['before' => ['/', 'setting/*', 'dashboard/*']],
+        'auth' => ['before' => ['/', 'setting/*', 'api/*', 'dashboard/*']],
     ];
 }
